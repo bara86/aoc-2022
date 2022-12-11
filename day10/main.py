cb3f3c0c3d4389c218516a13921145f548b04936
Bara <bara@develer.com>
2022-12-11 09:29:16 +0100

    Add day 10

--- day10/main.py
+++ day10/main.py
@@ -10,6 +10,23 @@ def main():
     total = 0
     is_right_moment = 20
 
+    rows = []
+    row = []
+
+    def addRow(row: list[str]) -> list[str]:
+
+        pixel_position = len(row)
+        if pixel_position in range(x - 1, x + 2):
+            row.append('#')
+        else:
+            row.append('.')
+
+        if len(row) == 40:
+            rows.append(row)
+            return []
+
+        return row
+
     for line in lines:
 
         if line.strip() == 'noop':
@@ -17,6 +34,7 @@ def main():
             if i % is_right_moment == 0:
                 total += i * x
                 is_right_moment += 40
+            row = addRow(row)
         else:
             to_add = int(line.split()[1])
 
@@ -25,9 +43,14 @@ def main():
                 if i % is_right_moment == 0:
                     total += i * x
                     is_right_moment += 40
+                row = addRow(row)
             x += to_add
 
-    return total
+    for row in rows:
+        print(f'{"".join(row)}')
+    print()
+
+    return total, 'BGKAEREZ'
 
 
 if __name__ == "__main__":
